5c9ee921529f3e4cab3ad25d1dc0cf69ae14b76a
Ramsey2022 <abovethelotus@yahoo.com>
2023-08-26 18:24:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
+
+const myLibrary = [];
+
+function addBookToLibrary() {
+
+}
