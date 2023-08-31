9d3650886052f3bb87696c355dbdb47bd28dd16c
Ramsey2022 <abovethelotus@yahoo.com>
2023-08-31 14:53:50 -0700

    set up book-form

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 class Book {
   constructor(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
   }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-
 const myLibrary = [];
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages, read) {
+  id = myLibrary.length + 1;
+  myLibrary.push(new Book(title, author, pages, read, id));
 }
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
